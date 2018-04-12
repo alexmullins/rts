e50b0f6f1049a08adff54fb124df5ec2b3f4fb17
Alex Mullins <jalexmullins91@gmail.com>
2018-04-12 14:17:56 -0500

    Added basic logger functions

---
+++ pilogger.py
@@ -0,0 +1,23 @@
+def logger_init():
+    pass
+
+# Events:
+#   MOTOR
+#   RFID
+#   ULTRASONIC
+#   LIGHT
+#   ENDRUN
+def logger_write_motor(speed):
+    pass
+
+def logger_write_rfid(rfid_id):
+    pass
+
+def logger_write_ultra(color):
+    pass
+
+def logger_write_light(color):
+    pass
+
+def logger_write_endrun(elapsed):
+    pass
